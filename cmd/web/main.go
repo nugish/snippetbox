b52c835416800950a666986e10a43e5196d6b7e6
nugish <godjangocode@gmail.com>
2022-12-10 21:33:01 +0700

    3.5 isolating the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,18 +24,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server %s", *addr)
